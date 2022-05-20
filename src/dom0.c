e71477ba1fbd900aac7eb28b7d15224292db5272
Dmytro Firsov <dmytro_firsov@epam.com>
2022-05-20 13:37:32 +0300

    domd: xenvm: Fix IRQ and dtdev sharing, add all required items for DomD
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- src/dom0.c
+++ src/dom0.c
@@ -22,14 +22,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define GIC_SPI_SHIFT		0x20
 #define LOAD_ADDR_OFFSET	0x80000
 
 static sys_dlist_t domain_list = SYS_DLIST_STATIC_INIT(&domain_list);
 
-#define NR_DOMD_DTDEVS 1
+/* Please, keep this define in sync with number of dtdev entries */
+#define NR_DOMD_DTDEVS 37
 static char *domd_dtdevs[] = {
+	"/soc/dma-controller@e6460000",
+	"/soc/dma-controller@e6470000",
+	"/soc/dma-controller@e6700000",
+	"/soc/dma-controller@e7300000",
+	"/soc/dma-controller@e7310000",
+	"/soc/dma-controller@ec700000",
+	"/soc/dma-controller@ec720000",
 	"/soc/ethernet@e6800000",
+	"/soc/dma-controller@e65a0000",
+	"/soc/dma-controller@e65b0000",
+	"/soc/mmc@ee100000",
+	"/soc/mmc@ee140000",
+	"/soc/usb@ee0a0100",
+	"/soc/usb@ee0a0000",
+	"/soc/imr-lx4@fe860000",
+	"/soc/imr-lx4@fe870000",
+	"/soc/imr-lx4@fe880000",
+	"/soc/imr-lx4@fe890000",
+	"/soc/vspm@fe920000",
+	"/soc/vspm@fe960000",
+	"/soc/vspm@fe9a0000",
+	"/soc/vspm@fe9b0000",
+	"/soc/fcp@fea27000",
+	"/soc/fcp@fea2f000",
+	"/soc/fcp@fea37000",
+	"/soc/fdpm@fe940000",
+	"/soc/fdpm@fe944000",
+	"/soc/hdmi@fead0000",
+	"/soc/gsx_pv0_domd",
+	"/soc/gsx_pv1_domd",
+	"/soc/gsx_pv2_domd",
+	"/soc/gsx_pv3_domd",
+	"/soc/fcpcs_vc0",
+	"/soc/fcpcs_vc1",
+	"/soc/impdm0",
+	"/soc/dma-controller@ffc10000",
+	"/soc/dma-controller@ffc20000",
 };
 
 static struct xen_domain_iomem domd_iomems[] = {
@@ -44,7 +80,196 @@ static struct xen_domain_iomem domd_iomems[] = {
 };
 
 static uint32_t domd_irqs[] = {
-	153, /* SCIF1 */
+	/* mfis-as */
+	212,
+	/* gpio@e6050000*/
+	36,
+	/* gpio@e6051000 */
+	37,
+	/* gpio@e6052000*/
+	38,
+	/* gpio@e6053000*/
+	39,
+	/* gpio@e6054000 */
+	40,
+	/* gpio@e6055000 */
+	41,
+	/* gpio@e6055400 */
+	42,
+	/* gpio@e6055800 */
+	43,
+	/* thermal@e6198000 */
+	99, 100, 101,
+	/* interrupt-controller@e61c0000 */
+	32, 33, 34, 35, 193, 50,
+	/* i2c@e6510000 */
+	318,
+	/*  i2c@e66d8000 */
+	51,
+	/* i2c@e60b0000 */
+	205,
+	/* serial@e6550000 */
+	187,
+	/* usb@e6590000 */
+	139,
+	/* TODO: add node to Xen device tree for this - usb@e659c000 */
+	/* 69, */
+	/* dma-controller@e65a0000 */
+	141,
+	/* dma-controller@e65b0000 */
+	142,
+	/* dma-controller@e6460000 */
+	66,
+	/* dma-controller@e6470000 */
+	67,
+	/* crypto@e6601000 */
+	103,
+	/* dma-controller@e6700000 */
+	231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247,
+	/* dma-controller@e7300000 */
+	251, 345, 252, 346, 248, 340, 341, 342, 343, 344, 249, 250, 347, 348, 349, 350, 351,
+	/* dma-controller@e7310000 */
+	448, 449, 450, 451, 452, 453, 454, 455, 456, 457, 458, 459, 460, 461, 462, 463, 429,
+	/* ethernet@e6800000 */
+	71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
+	/* rpc0@ee200000 */
+	70,
+	/* video@e6ef0000 */
+	220,
+	/* video@e6ef1000 */
+	221,
+	/* video@e6ef2000 */
+	222,
+	/* video@e6ef3000 */
+	223,
+	/* video@e6ef4000 */
+	206,
+	/* video@e6ef5000 */
+	207,
+	/* video@e6ef6000 */
+	208,
+	/* video@e6ef7000 */
+	203,
+	/* src-0 */
+	384,
+	/* src-1 */
+	385,
+	/* src-2 */
+	386,
+	/* src-3 */
+	387,
+	/* src-4 */
+	388,
+	/* src-5 */
+	389,
+	/* src-6 */
+	390,
+	/* src-7 */
+	391,
+	/* src-8 */
+	392,
+	/* src-9 */
+	393,
+	/* ssi-0 */
+	402,
+	/* ssi-1 */
+	403,
+	/* ssi-2 */
+	404,
+	/* ssi-3 */
+	405,
+	/* ssi-4 */
+	406,
+	/* ssi-5 */
+	407,
+	/* ssi-6 */
+	408,
+	/* ssi-7 */
+	409,
+	/* ssi-8 */
+	410,
+	/* ssi-9 */
+	411,
+	/* dma-controller@ec700000 */
+	352, 353, 354, 355, 356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367, 382,
+	/* dma-controller@ec720000 */
+	383, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 379, 380, 381, 414, 415,
+	/* adsp@ec800000 */
+	/*   269, */
+	/* usb@ee000000 */
+	134,
+	/* usb@ee020000 */
+	136,
+	/* usb@ee080000 */
+	140,
+	/* usb@ee0a0000 */
+	144,
+	/* usb@ee0c0000 */
+	145,
+	/* TODO: add node to Xen device tree for this - usb@ee0e0000 */
+	/* 68, */
+	/* mmc@ee100000 */
+	197,
+	/* mmc@ee140000 */
+	199,
+	/* mmc@ee160000 */
+	200,
+	/* gsx@fd000000 */
+	151,
+	/* pcie@fe000000 */
+	148, 149, 150,
+	/* pcie@ee800000 */
+	180, 181, 182,
+	/* imr-lx4@fe860000 */
+	224,
+	/* imr-lx4@fe870000 */
+	225,
+	/* imr-lx4@fe880000 */
+	226,
+	/* imr-lx4@fe890000 */
+	227,
+	/* vcp4@fe8d0000 */
+	412, 413, 255,
+	/* vcp4@fe910000 */
+	292, 293,
+	/* vcp4@fe900000 */
+	272, 273,
+	/* fdpm@fe940000 */
+	294,
+	/* fdpm@fe944000 */
+	295,
+	/* vspm@fe960000 */
+	298,
+	/* vspm@fe920000 */
+	497,
+	/* vsp@fea20000 */
+	498,
+	/* vsp@fea28000 */
+	499,
+	/* vsp@fea30000 */
+	500,
+	/* vspm@fe9a0000 */
+	476,
+	/* vspm@fe9b0000 */
+	477,
+	/* csi2@fea80000 */
+	216,
+	/* csi2@feaa0000 */
+	278,
+	/* hdmi@fead0000 */
+	421,
+	/* hdmi@feae0000 */
+	468,
+	/* display@feb00000 */
+	288, 300, 301, 302,
+	/* impdes0 */
+	471,
+	/* imprtt */
+	425,
+	/* dma-controller@ffc10000 */
+	480, 481, 482, 483, 484, 485, 486, 487, 488,
+	/* dma-controller@ffc20000 */
+	501, 489, 490, 491, 492, 493, 494, 495, 496,
 };
 
 static struct xen_domain_cfg domd_cfg = {
@@ -79,14 +304,14 @@ static void arch_prepare_domain_cfg(struct xen_domain_cfg *dom_cfg,
 
 	/*
 	 * xen_arch_domainconfig 'nr_spis' should be >= than biggest
-	 * irq num + 0x20 GIC SPI offset
+	 * absolute irq number.
 	 */
 	for (i = 1; i < dom_cfg->nr_irqs; i++) {
 		if (max_irq < dom_cfg->irqs[i]) {
 			max_irq = dom_cfg->irqs[i];
 		}
 	}
-	arch_cfg->nr_spis = max_irq + GIC_SPI_SHIFT;
+	arch_cfg->nr_spis = max_irq;
 }
 
 static void prepare_domain_cfg(struct xen_domain_cfg *dom_cfg,
@@ -331,15 +556,13 @@ int share_domain_iomems(int domid, struct xen_domain_iomem *iomems, int nr_iomem
 int bind_domain_irqs(int domid, uint32_t *irqs, int nr_irqs)
 {
 	int i, rc = 0;
-	uint32_t shifted_irq;
 
 	for (i = 0; i < nr_irqs; i++) {
-		shifted_irq = irqs[i] + GIC_SPI_SHIFT;
-		rc = xen_domctl_bind_pt_irq(domid, shifted_irq, PT_IRQ_TYPE_SPI,
-				0, 0, 0, 0, shifted_irq);
+		rc = xen_domctl_bind_pt_irq(domid, irqs[i], PT_IRQ_TYPE_SPI,
+				0, 0, 0, 0, irqs[i]);
 		if (rc) {
 			printk("Failed to bind irq #%d, err = %d\n", irqs[i], rc);
-			return rc;
+			/*return rc;*/
 		}
 	}
 	return rc;
@@ -478,6 +701,7 @@ int domu_create(const struct shell *shell, size_t argc, char **argv)
 
 	memset(&config, 0, sizeof(config));
 	prepare_domain_cfg(&domd_cfg, &config);
+	config.grant_opts = 1;
 	rc = xen_domctl_createdomain(domid, &config);
 	printk("Return code = %d creation\n", rc);
 	if (rc) {
@@ -517,7 +741,7 @@ int domu_create(const struct shell *shell, size_t argc, char **argv)
 	rc = bind_domain_irqs(domid, domd_cfg.irqs, domd_cfg.nr_irqs);
 
 	/* TODO: fix it */
-	rc = assign_dtdevs(domid, domd_dtdevs, 1);
+	rc = assign_dtdevs(domid, domd_dtdevs, domd_cfg.nr_dtdevs);
 
 	memset(&vcpu_ctx, 0, sizeof(vcpu_ctx));
 	vcpu_ctx.user_regs.x0 = dtb_addr;
