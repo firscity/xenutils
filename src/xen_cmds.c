02a5c722b79587da5703bbc560aa9a5d9d2e4f1e
Mykola Perehudov <Mykola_Perehudov@epam.com>
2022-10-12 15:29:22 +0300

    shell: Fix arguments configuration
    Signed-off-by: Mykola Perehudov <Mykola_Perehudov@epam.com>

--- src/xen_cmds.c
+++ src/xen_cmds.c
@@ -10,20 +10,20 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 	SHELL_CMD_ARG(create, NULL,
 		      " Create Xen domain\n"
 		      " Usage: create -d domid\n",
-		      domu_create, 3, 1),
+		      domu_create, 3, 0),
 	SHELL_CMD_ARG(destroy, NULL,
 		      " Destroy Xen domain\n"
 		      " Usage: destroy -d domid\n",
-		      domu_destroy, 3, 1),
+		      domu_destroy, 3, 0),
 	SHELL_CMD_ARG(console_start, NULL,
 		      " Start console thread for Xen domain\n"
 		      " Only single thread and only output is currently supported\n"
 		      " Usage: console_start -d domid\n",
-		      domu_console_start, 3, 1),
+		      domu_console_start, 3, 0),
 	SHELL_CMD_ARG(console_stop, NULL,
 		      " Stop console thread for Xen domain\n"
 		      " Usage: console_stop\n",
-		      domu_console_stop, 1, 1),
+		      domu_console_stop, 3, 0),
 	SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_ARG_REGISTER(xu, &subcmd_xu, "Xenutils commands", NULL, 2, 0);
