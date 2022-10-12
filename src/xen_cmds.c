91aec29f8d461150b3b1b2ce45d10d07132bbab2
Mykola Perehudov <Mykola_Perehudov@epam.com>
2022-10-12 15:30:37 +0300

    shell: Update commands usage description
    Signed-off-by: Mykola Perehudov <Mykola_Perehudov@epam.com>

--- src/xen_cmds.c
+++ src/xen_cmds.c
@@ -9,20 +9,20 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 	subcmd_xu,
 	SHELL_CMD_ARG(create, NULL,
 		      " Create Xen domain\n"
-		      " Usage: create -d domid\n",
+		      " Usage: create -d <domid>\n",
 		      domu_create, 3, 0),
 	SHELL_CMD_ARG(destroy, NULL,
 		      " Destroy Xen domain\n"
-		      " Usage: destroy -d domid\n",
+		      " Usage: destroy -d <domid>\n",
 		      domu_destroy, 3, 0),
 	SHELL_CMD_ARG(console_start, NULL,
 		      " Start console thread for Xen domain\n"
 		      " Only single thread and only output is currently supported\n"
-		      " Usage: console_start -d domid\n",
+		      " Usage: console_start -d <domid>\n",
 		      domu_console_start, 3, 0),
 	SHELL_CMD_ARG(console_stop, NULL,
 		      " Stop console thread for Xen domain\n"
-		      " Usage: console_stop\n",
+		      " Usage: console_stop -d <domid>\n",
 		      domu_console_stop, 3, 0),
 	SHELL_SUBCMD_SET_END);
 
